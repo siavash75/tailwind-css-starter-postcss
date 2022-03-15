3b47f553af2ccb36a64d30027115ac76a566e773
siavash75 <39434737+siavash75@users.noreply.github.com>
2022-03-15 19:36:07 +0330

    add file icons
    add icon to project

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,17 @@
+const colors = require("tailwindcss/colors");
+
 module.exports = {
   purge: {
     content: ['./public/**/*.html'],
   },
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+      'cyan':colors.cyan,
+      'teal':colors.teal
+      }
+    },
   },
   variants: {
     extend: {},
